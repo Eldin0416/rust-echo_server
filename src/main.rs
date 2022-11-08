7c3ab6c63c3c5acd0ad3bb2198b37ec2cc82a61d
SeungMin Cha <eldin0416@gmail.com>
2022-11-08 15:26:58 +0900

    commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::io::{Read, Write};
+
+fn handle_client(mut stream: TcpStream) {
+    //read 20 bytes at a time from stream echoing back to stream
+    loop {
+        let mut read = [0; 1028];
+        match stream.read(&mut read) {
+            Ok(n) => {
+                if n == 0 {
+                    //connection was closed
+                    println!("Closed connection");
+                    match stream.peer_addr() {
+                        Ok(peer) => {
+                           println!("peer_addr {}", peer.ip());
+                        },
+                        Err(e) => {
+                            println!("{}", e);
+                        }
+                    }
+                    break;
+                }
+                stream.write(&read[0..n]).unwrap();
+            }
+            Err(err) => {
+                panic!("{}", err);
+            }
+        }
+    }
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream);
+                });
+            }
+            Err(_) => {
+                println!("Error");
+            }
+        }
+    }
+}
